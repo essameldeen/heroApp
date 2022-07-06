68351c8f02005a8e16500f8e6a2c284ae539b4e1
essameldeen <essameldeen.ibrahim.stdsw@cis.asu.edu.eg>
2022-07-06 12:29:33 +0200

    hero-datasource module [ heroDto]

--- hero/hero-datasource/build.gradle.kts
+++ hero/hero-datasource/build.gradle.kts
@@ -1,6 +1,15 @@
 apply {
     from("$rootDir/library-build.gradle")
 }
+plugins {
+    kotlin(KotlinPlugins.serialization) version Kotlin.version
+    id(SqlDelight.plugin)
+}
 dependencies {
 
+    "implementation"(project(Modules.heroDomain))
+
+    "implementation"(Ktor.core)
+    "implementation"(Ktor.clientSerialization)
+    "implementation"(Ktor.android)
 }
---
+++ hero/hero-datasource/src/main/java/com/vodeg/hero_datasource/network/Endpoints.kt
@@ -0,0 +1,6 @@
+package com.vodeg.hero_datasource.network
+
+object Endpoints {
+    const val BASE_URL = "https://api.opendota.com"
+    const val HERO_STATS = "$BASE_URL/api/heroStats"
+}
