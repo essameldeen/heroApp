fc6a2077f039d893c38330fa2e69356bf8ffe863
essameldeen <essameldeen.ibrahim.stdsw@cis.asu.edu.eg>
2022-07-06 13:57:37 +0200

    hero-datasource module [ hero service - http client ktor ]

---
+++ hero/hero-datasource/src/main/java/com/vodeg/hero_datasource/network/HeroService.kt
@@ -0,0 +1,29 @@
+package com.vodeg.hero_datasource.network
+
+import com.vodeg.hero_domain.Hero
+import io.ktor.client.*
+import io.ktor.client.engine.android.*
+import io.ktor.client.features.json.*
+import io.ktor.client.features.json.serializer.*
+
+interface HeroService {
+
+    suspend fun getHeroStats(): List<Hero>
+
+    companion object Factory {
+
+        fun build(): HeroService {
+            return HeroServiceImpl(
+                httpClient = HttpClient(Android) {
+                    install(JsonFeature) {
+                        serializer = KotlinxSerializer(
+                            kotlinx.serialization.json.Json {
+                                ignoreUnknownKeys = true
+                            }
+                        )
+                    }
+                }
+            )
+        }
+    }
+}
---
+++ hero/hero-datasource/src/main/java/com/vodeg/hero_datasource/network/HeroServiceImpl.kt
@@ -0,0 +1,16 @@
+package com.vodeg.hero_datasource.network
+
+import com.vodeg.hero_domain.Hero
+import io.ktor.client.*
+import io.ktor.client.request.*
+
+class HeroServiceImpl(
+    private val httpClient: HttpClient
+) : HeroService {
+    override suspend fun getHeroStats(): List<Hero> {
+        return httpClient.get<List<HeroDto>>() {
+            url(Endpoints.HERO_STATS)
+        }.map { it.toHero() }
+    }
+
+}
